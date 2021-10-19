0a725ed5411e5e94347c77e91e297c68f1545e78
manekajal <manekajal2014@gmail.com>
2021-10-19 15:33:31 +0530

    [kajal]:Read or Write the address book with contact into file IO

--- src/main/java/com/bl/addressbook/controller/AddressBookController.java
+++ src/main/java/com/bl/addressbook/controller/AddressBookController.java
@@ -13,48 +13,12 @@ public class AddressBookController {
 
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Contact> arrayList = new ArrayList<Contact>();
 
-
-        boolean flag = true;
-
-        AddressBookService addbook = new AddressBookService();
-
-
-            while (flag) {
-
-                System.out.println("Welcome to Address Book");
-                System.out.println("Enter 1 for add, 2 for edit, 3 for delete,4 for print, 5 for search, 6 for view City , 7 for exit");
-                System.out.println("Enter your Choice :");
-                switch (scanner.nextInt()) {
-                    case 1:
-                        addbook.addData();
-                        System.out.println();
-                        break;
-                    case 2:
-
-                        addbook.edit();
-                        break;
-                    case 3:
-                        addbook.delete();
-                        System.out.println("________________");
-                        break;
-                    case 4:
-                        addbook.showData();
-
-                        break;
-                    case 5:
-                        addbook.search();
-                        System.out.println("________________");
-                        break;
-                    case 6:
-                        addbook.viewCity();
-                    case 7:
-                        flag = false;
-                        break;
-                }
-            }
+            ArrayList<Contact> contacts=new ArrayList<>();
+            AddressBookService addressBookService=new AddressBookService(contacts);
+            Scanner consoleInputReader = new Scanner(System.in);
+            addressBookService.readEmployeePayrollData(consoleInputReader);
+            addressBookService.writeEmployeePayrollData();
 
         }
     }
--- src/main/java/com/bl/addressbook/service/AddressBookService.java
+++ src/main/java/com/bl/addressbook/service/AddressBookService.java
@@ -3,193 +3,56 @@ package com.bl.addressbook.service;
 import com.bl.addressbook.models.Contact;
 
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class AddressBookService {
-    ArrayList<Contact> arrayList = new ArrayList<Contact>();
+    private static List<Contact> contact;
 
+    private static List<Contact> addressBookServiceList;
 
-    Scanner scanner;
+    public enum IOService{console_IO,FILE_IO,DB_IO,REST_IO};
 
     public AddressBookService() {
-        scanner = new Scanner(System.in);
-    }
-
-    public void addData() {
-        System.out.println("Enter all the details");
-
-        System.out.print(" Enter First Name : ");
-        String firstName = scanner.nextLine();
-
-        System.out.print(" Enter Last Name : ");
-        String lastName = scanner.nextLine();
-        System.out.print("Enter Address : ");
-        String address = scanner.nextLine();
-        System.out.print("Enter City : ");
-        String city = scanner.nextLine();
-
-        System.out.print("Enter state : ");
-        String state = scanner.nextLine();
-        System.out.print("Enter zip : ");
-        String zipCode = scanner.nextLine();
-        System.out.print("Enter Mobile Number : ");
-        String mobileNo = scanner.nextLine();
-        System.out.print("Enter the Email ID : ");
-        String emailId = scanner.nextLine();
-        for(int i=0;i<arrayList.size();i++){
-
-            if(arrayList.get(i).getFirstName().equals(firstName))
-            {
-                System.out.println(" Sorry Name is already Exist :");
-                return;
-            }
-
-        }
-        arrayList.add(new Contact(firstName, lastName, address, city, state, zipCode, mobileNo, emailId));
-        System.out.println("Data added Successfully");
-
 
     }
 
-    public void showData() {
-        for(int i =0 ;i<arrayList.size();i++){
-            System.out.println(arrayList.get(i).getFirstName());
-            System.out.println(arrayList.get(i).getLastName());
-            System.out.println(arrayList.get(i).getAddress());
-            System.out.println(arrayList.get(i).getCity());
-            System.out.println(arrayList.get(i).getZipCode());
-            System.out.println(arrayList.get(i).getState());
-            System.out.println(arrayList.get(i).getMobileNo());
-            System.out.println(arrayList.get(i).getEmailId());
-        }
+    public AddressBookService(List<Contact> addressBookServiceList){
+        this.addressBookServiceList =addressBookServiceList;
 
     }
 
-    public void delete() {
-        boolean found = false;
-        System.out.println(" Enter the name to search :");
-        String firstName = scanner.nextLine();
-        System.out.println("_____________________________");
-        Iterator<Contact> it = arrayList.iterator();
-        while (it.hasNext()) {
-            Contact contact = it.next();
-            if (contact.getFirstName().equals(firstName)) {
-               it.remove();
-                found = true;
-            }
 
 
-            if (!found) {
-                System.out.println(" name is not found");
-            }else
-            {
-                System.out.println(" Record is deleted Successfully :");
-            }
-            break;
-        }
+
+
+    public void readEmployeePayrollData(Scanner consoleInputReader) {
+
+
+        System.out.println(" Enter Employee First Name :");
+        String firstName= consoleInputReader.next();
+        System.out.println(" Enter Employee Last Name :");
+        String lastName= consoleInputReader.next();
+        System.out.println(" Enter Employee Address :");
+        String address= consoleInputReader.next();
+        System.out.println(" Enter Employee City :");
+        String city= consoleInputReader.next();
+        System.out.println(" Enter Employee State :");
+        String state= consoleInputReader.next();
+        System.out.println(" Enter Employee Zip Code :");
+        String zipCode= consoleInputReader.next();
+        System.out.println(" Enter Employee Mobile Number :");
+        String mobileNo= consoleInputReader.next();
+        System.out.println(" Enter Employee Email ID :");
+        String emailId= consoleInputReader.next();
+
+        addressBookServiceList.add(new Contact(firstName,lastName,address,city,state,zipCode,mobileNo,emailId));
 
     }
+    public void writeEmployeePayrollData() {
 
-    public void edit() {
-        boolean found = false;
-        System.out.println(" Enter the name to edit :");
-        String firstName = scanner.nextLine();
-        System.out.println("_____________________________");
-        ListIterator<Contact>li= arrayList.listIterator();
+        System.out.println(" Writing Employee Payroll Roaster to console \n" +addressBookServiceList);
 
-        while (li.hasNext()) {
-            Contact contact = li.next();
-            if (contact.getFirstName().equals(firstName)) {
-                System.out.println("Enter all the details");
-
-                System.out.print(" Enter First Name : ");
-                String fName = scanner.nextLine();
-
-                System.out.print(" Enter Last Name : ");
-                String lastName = scanner.nextLine();
-                System.out.print("Enter Address : ");
-                String address = scanner.nextLine();
-                System.out.print("Enter City : ");
-                String city = scanner.nextLine();
-
-                System.out.print("Enter state : ");
-                String state = scanner.nextLine();
-                System.out.print("Enter zip : ");
-                String zipCode = scanner.nextLine();
-                System.out.print("Enter Mobile Number : ");
-                String mobileNo = scanner.nextLine();
-                System.out.print("Enter the Email ID : ");
-                String emailId = scanner.nextLine();
-
-                li.set(new Contact(firstName, lastName, address, city, state, zipCode, mobileNo, emailId));
-
-
-
-                found = true;
-            }
-
-
-            if (!found) {
-                System.out.println(" name is not found");
-            }else
-            {
-                System.out.println(" Record is edited Successfully :");
-            }
-            break;
-        }
-    }
-    public void search() {
-        boolean found = false;
-
-        System.out.println(" Enter the city to search :");
-        String city = scanner.nextLine();
-        System.out.println(" Enter the state");
-        String state = scanner.nextLine();
-        System.out.println("_____________________________");
-        Iterator<Contact> it = arrayList.iterator();
-        while (it.hasNext()) {
-            Contact contact = it.next();
-            if (contact.getCity().equals(city)) {
-                System.out.println(contact);
-
-                found = true;
-            }
-            if (contact.getState().equals(state)) {
-                System.out.println(contact);
-
-                found = true;
-            }
-
-            if (!found) {
-                System.out.println(" city is not found");
-            }
-            break;
-        }
-
-
-  //      public void viewCity();
-  //      {
-  //          System.out.println("Enter the City or State");
-   //        String  cityOrState = scanner.nextLine();
-  //          arrayList.stream()
-   //                 .map(Contact::getCity)
-   //                 .distinct()
-     //               .forEach(System.out::println);
-//
-
-  //      }
-    }
-
-    public void viewCity(){
-        System.out.println("Enter the City or State");
-        String  cityOrState = scanner.nextLine();
-        List<Contact> contactList1 = new ArrayList<>();
-        arrayList.stream()
-                .map(Contact::getCity)
-                .distinct()
-                .forEach(System.out::println);
     }
 
 }
-
-    
